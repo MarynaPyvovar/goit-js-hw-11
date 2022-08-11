b04091eab7aa1cfe6e122adf241db424dd2d6af5
MarynaPyvovar <marina18renkas@gmail.com>
2022-08-11 16:54:13 +0300

    feat add loadMore

--- src/index.js
+++ src/index.js
@@ -12,48 +12,51 @@ const refs = {
 
 const API_KEY = '29177846-79ba9349bc407ca7d45dbb10e';
 const BASE_URL = 'https://pixabay.com/api';
+let page = 0;
+let totalInfo = 0;
 
-refs.form.addEventListener('submit', onSubmitClick);
-// refs.loadMoreBtn.addEventListener('click', onLoadMoreClick);
-
-refs.loadMoreBtn.style.display = 'none';
-refs.gallery.style.display = 'flex';
-refs.gallery.style.flexWrap = 'wrap';
-refs.gallery.style.justifyContent = 'space-around';
-
-function onSubmitClick(e) {
+const onSubmitClick = async e => {
   e.preventDefault();
+  refs.loadMoreBtn.style.display = 'none';
   refs.gallery.innerHTML = '';
+  page = 1;
+  totalInfo = 40;
 
-  renderGallery();
+  const data = await fetchItems(refs.input.value);
+  if (data) {
+    await renderGallery(data);
+    Notiflix.Notify.info(`Hooray! We found ${data.totalHits} images.`);
+    // await lightbox.refresh();
+    const lightbox = new SimpleLightbox('.gallery a');
+  }
+};
 
-  refs.loadMoreBtn.style.display = 'block';
-}
-
-const fetchItems = async searchQuery => {
+const fetchItems = async query => {
+  const searchQuery = query.trim();
   try {
     if (searchQuery) {
       const responce = await axios.get(
-        `${BASE_URL}/?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true$per_page=40`
+        `${BASE_URL}/?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`
       );
-      if (responce.data.totalHits === 0) {
+      if (responce.data.total === 0) {
         Notiflix.Notify.failure(
           'Sorry, there are no images matching your search query. Please try again.'
         );
         return;
       }
+      refs.loadMoreBtn.style.display = 'block';
       return responce.data;
+    } else {
+      Notiflix.Notify.failure('Enter your search query.');
     }
   } catch (error) {
     console.log(error);
   }
 };
 
-const renderGallery = async () => {
+const renderGallery = async data => {
   try {
-    const data = await fetchItems(refs.input.value);
-    console.log(data.hits);
-    const gallery = await data.hits
+    const gallery = data?.hits
       .map(
         ({
           webformatURL,
@@ -86,11 +89,24 @@ const renderGallery = async () => {
       )
       .join('');
     refs.gallery.insertAdjacentHTML('beforeend', gallery);
-
-    const lightbox = new SimpleLightbox('.gallery a');
   } catch (error) {
     console.log(error);
   }
 };
 
-// const onLoadMoreClick = async () => {}
+const onLoadMoreClick = async () => {
+  page += 1;
+  const data = await fetchItems(refs.input.value);
+  await renderGallery(data);
+  totalInfo += 40;
+  console.log(totalInfo);
+  if (data.totalHits >= totalInfo) {
+    refs.loadMoreBtn.style.display = 'none';
+    Notiflix.Notify.info(
+      "We're sorry, but you've reached the end of search results."
+    );
+  }
+};
+
+refs.form.addEventListener('submit', onSubmitClick);
+refs.loadMoreBtn.addEventListener('click', onLoadMoreClick);
